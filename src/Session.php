fb71fca7ed0a7fe27efb68d7c4bcfd869c9ad94a
Mẫn Văn Hậu <manhau.174@gmail.com>
2020-02-25 10:14:55 +0700

    baokim sdk

--- src/Session.php
+++ src/Session.php
@@ -3,11 +3,24 @@ namespace BaokimSDK;
 require_once(__DIR__ . '/../vendor/autoload.php');
 require_once(__DIR__ . '/../config/config.php');
 use BaokimSDK\BaoKim;
-use \GuzzleHttp\Client;
+use GuzzleHttp\Client;
 
 class Session {
     const ERR_NONE = 0;
 
+    /**
+     * Create session checkout
+     * @param $data["mrc_order_id"] string required
+     * @param $data["payment_method_types"] array required [1,2,3]
+     * @param $data["line_items"] array required
+     * @param $data["success_url"] string|url required
+     * @param $data["cancel_url"] string|url optional
+     * @param $data["webhook_url"] string|url required
+     * @param $data["customer_email"] string|email required
+     * @param $data["customer_phone"] string required
+     * @return mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
     public static function create($data) {
         $client = new Client(['timeout' => 20.0]);
         $options['query']['jwt'] = BaoKim::$_jwt;
